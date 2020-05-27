80165d8d9e0f27a194d0c56869582be335c62922
donatom <manuel.donato@tasgroup.eu>
2020-05-27 17:42:26 +0200

    converter refactor + additional tests

--- src/main/java/eu/tasgroup/hyperskill/jsonparser/utils/JSONUtils.java
+++ src/main/java/eu/tasgroup/hyperskill/jsonparser/utils/JSONUtils.java
@@ -1,9 +1,13 @@
 package eu.tasgroup.hyperskill.jsonparser.utils;
 
+import eu.tasgroup.hyperskill.jsonparser.model.JSONElement;
+
 import java.security.InvalidParameterException;
+import java.util.List;
 import java.util.Objects;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public class JSONUtils {
 
@@ -55,14 +59,14 @@ public class JSONUtils {
     }
 
     public static boolean isValidKey(String key) {
-    	
-    	Objects.requireNonNull(key, JSON_STRING_CANNOT_BE_NULL);
 
-        if (key.equals("@")) 
+        Objects.requireNonNull(key, JSON_STRING_CANNOT_BE_NULL);
+
+        if (key.equals("@"))
             return false;
-        if (key.equals("")) 
+        if (key.equals(""))
             return false;
-        if (key.equals("#")) 
+        if (key.equals("#"))
             return false;
         return true;
     }
@@ -73,4 +77,32 @@ public class JSONUtils {
         return originalKey.replaceFirst("^[@#]", "");
     }
 
+    public static boolean hasNoChildren(JSONElement jsonElement) {
+        return jsonElement.getChildren().isEmpty();
+    }
+
+    public static boolean hasExactlyOneChildWithHashKey(JSONElement jsonElement) {
+
+        List<JSONElement> l = jsonElement.getChildren().stream().filter(child -> child.getKey().startsWith("#")).collect(Collectors.toList());
+
+        return l.size() == 1 && JSONUtils.getNormalizedKey(l.get(0).getKey()).equals(jsonElement.getKey());
+
+        /*return jsonElement.getChildren().size() == 1
+                && jsonElement.getChildren().get(0).getKey().startsWith("#")
+                && getNormalizedKey(jsonElement.getChildren().get(0).getKey()).equals(jsonElement.getKey());*/
+    }
+
+    public static boolean hasChildWithoutSpecialCharacter(JSONElement jsonElement) {
+
+        return jsonElement.getChildren().stream()
+                .anyMatch(jsonChild -> !jsonChild.getKey().startsWith("@") && !jsonChild.getKey().startsWith("#"));
+    }
+
+    public static boolean hasEmptyKey(JSONElement jsonElement) {
+        return jsonElement.getChildren().stream().anyMatch(child -> child.getKey().equals("#") || child.getKey().equals("@"));
+    }
+
+    public static boolean hasChildlessChildren(JSONElement e) {
+        return e.getChildren().stream().anyMatch(child -> !hasNoChildren(child));
+    }
 }
---
+++ src/main/java/eu/tasgroup/hyperskill/jsonparser/visitor/JSONConverter2.java
@@ -0,0 +1,95 @@
+package eu.tasgroup.hyperskill.jsonparser.visitor;
+
+import eu.tasgroup.hyperskill.jsonparser.model.JSONElement;
+import eu.tasgroup.hyperskill.jsonparser.model.XMLElement;
+import eu.tasgroup.hyperskill.jsonparser.utils.JSONUtils;
+
+import java.util.*;
+
+public class JSONConverter2 {
+
+    public static final String JSON_ELEMENT_CAN_T_BE_NULL = "JSON element can't be null";
+
+    public XMLElement convert(JSONElement jsonElement) {
+
+        Objects.requireNonNull(jsonElement, JSON_ELEMENT_CAN_T_BE_NULL);
+
+        XMLElement xmlElement = new XMLElement();
+
+
+        xmlElement.setTagName(JSONUtils.getNormalizedKey(jsonElement.getKey())); //SETTO TAG NAME ---> L'HO MESSO QUI PERCHE' LO FA SEMPRE!!!!!
+
+        if (JSONUtils.hasNoChildren(jsonElement)) {
+            xmlElement.setText(jsonElement.getValue().toString());
+            return xmlElement;
+        } else if (isCorretlyFormattedXMLElement(jsonElement)) {
+            checkForText(xmlElement, jsonElement); //CONTROLLO SUL TEXT
+            checkForAttributesToInsert(xmlElement, jsonElement); //CONTROLLO SUGLI ATTRIBUTI (VIENE ESEGUITO ANCHE SE NON CI SONO
+        } else {
+
+            Set<JSONElement> set=checkForDuplicates(jsonElement);
+            jsonElement.getChildren().removeAll(set); //RIMUOVE GLI EVENTUALI DUPLICATI
+
+            jsonElement.getChildren().stream().filter(child -> !illegalInitialCharacter(child.getKey())).forEach(child -> xmlElement.addChild(convert(child)));
+            if (xmlElement.getChildren().isEmpty()) {
+                xmlElement.setText("");
+            }
+        }
+        return xmlElement;
+    }
+
+    private boolean isCorretlyFormattedXMLElement(JSONElement e) {
+        return JSONUtils.hasExactlyOneChildWithHashKey(e)
+                && !JSONUtils.hasChildWithoutSpecialCharacter(e)
+                && !JSONUtils.hasEmptyKey(e)
+                && !JSONUtils.hasChildlessChildren(e);
+    }
+
+    private void checkForText(XMLElement xe, JSONElement je) {
+
+        Optional<JSONElement> optional = je.getChildren().stream().filter(child -> child.getKey().startsWith("#")).findFirst();
+
+        if (JSONUtils.hasNoChildren(optional.get()) && optional.get().getValue() != null) {
+            xe.setText(optional.get().getValue().toString());
+        }
+    }
+
+    private String setValueXML(JSONElement e) {
+
+        Optional<Object> nullableValue = Optional.ofNullable(e.getValue());
+        return nullableValue.isPresent() ? e.getValue().toString() : "null";
+    }
+
+
+    private void checkForAttributesToInsert(XMLElement xmlElement, JSONElement jsonElement) { //VIENE ESEGUITO ANCHE SE I FIGLI CON LA CHIOCCIOLA NON CI SONO
+        jsonElement.getChildren().stream()
+                .filter(child -> child.getKey().startsWith("@"))
+                .forEach(child -> xmlElement.insertAttributeEntry(JSONUtils.getNormalizedKey(child.getKey()), setAttributesValueToXML(child)));
+    }
+
+    private String setAttributesValueToXML(JSONElement e) {
+
+        Optional<Object> nullableValue = Optional.ofNullable(e.getValue());
+        return nullableValue.isPresent() ? e.getValue().toString() : "";
+    }
+
+    private boolean illegalInitialCharacter(String key) {
+        return key.equals("@") || key.equals("#") || key.isEmpty(); //TENIAMO L'ISEMPTY() PER SALVARE L'OGGETTO XML O NO?
+    }
+
+    private Set<JSONElement> checkForDuplicates(JSONElement jsonElement) {
+
+        Set<JSONElement> set = new HashSet<>(); //evito eventuali duplicati
+
+        jsonElement.getChildren().stream().filter(el -> (el.getKey().startsWith("@") || el.getKey().startsWith("#")) && el.getKey().length() > 1)
+                .forEach(el -> {
+                    for (JSONElement j : jsonElement.getChildren()) {
+                        if (JSONUtils.getNormalizedKey(el.getKey()).equals(j.getKey())) {
+                            //AGGIUNGILO ALLA LISTA L DICHIARATA IN QUESTO METODO
+                            set.add(el);
+                        }
+                    }
+                });
+        return set;
+    }
+}
