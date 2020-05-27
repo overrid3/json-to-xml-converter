1da3ef71596a8ca65233b9148862745a7a202148
pinnag <greta.pinna@tasgroup.eu>
2020-05-27 10:16:34 +0200

    visitor

--- src/main/java/eu/tasgroup/hyperskill/jsonparser/visitor/JSONElementVisitor.java
+++ src/main/java/eu/tasgroup/hyperskill/jsonparser/visitor/JSONElementVisitor.java
@@ -1,13 +1,59 @@
 package eu.tasgroup.hyperskill.jsonparser.visitor;
 
-import eu.tasgroup.hyperskill.jsonparser.model.JSONElement;
-import eu.tasgroup.hyperskill.jsonparser.model.TreeElement;
 import eu.tasgroup.hyperskill.jsonparser.model.XMLElement;
 
 public class JSONElementVisitor {
 
-	public String print(JSONElement jsonElement){
-		return null;
+	String elementToString = "";
+	String path = "";
+
+	public String printElement(XMLElement element){
+		
+	//	Objects.requireNonNull(element, Constants.XMLOBJECT_CANNOT_BE_NULL.getConstant());
+
+		if(element.getTagName().equals("root")){
+				for(XMLElement elem: element.getChildren()){
+					elementToString = printElement(elem);
+					path="";
+				}
+		}
+		else{
+			//path da stampare
+			if(path.equals(""))
+				path=element.getTagName();
+			else
+				path += ", " + element.getTagName();
+			elementToString += "\nElement:\npath = " + path + "\n";
+
+			//l'elemento ha figli
+			if(!element.getChildren().isEmpty()){
+				//l'elemento ha attributi
+			//	if(element.checkAttributes())
+			//		printAttributes(element);
+				for(XMLElement elementSon : element.getChildren()){
+					elementToString = printElement(elementSon);
+					path = path.replaceFirst(", "+elementSon.getTagName(), "");
+				}
+			}
+			else{
+				//stampa valore
+				printValue(element);
+				//stampa attributi
+//				if(element.checkAttributes()){
+//					printAttributes(element);
+//				}
+			}
+		}
+		return elementToString;
 	}
+
+	public void printValue(XMLElement element){
+		if(element.getText().equals("null"))
+			elementToString += "value = "+element.getText()+"\n";
+		else{
+			elementToString += "value = \""+element.getText()+"\"\n";
+		}
+	}
+
 
 }
