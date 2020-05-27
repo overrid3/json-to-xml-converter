2d54becee65d907a18483a46dd3e4656cf0b2379
pinnag <greta.pinna@tasgroup.eu>
2020-05-27 18:17:12 +0200

    visitor + test

--- src/main/java/eu/tasgroup/hyperskill/jsonparser/visitor/JSONElementVisitor.java
+++ src/main/java/eu/tasgroup/hyperskill/jsonparser/visitor/JSONElementVisitor.java
@@ -1,25 +1,30 @@
 package eu.tasgroup.hyperskill.jsonparser.visitor;
 
+import java.util.Map;
+import java.util.Objects;
+
 import eu.tasgroup.hyperskill.jsonparser.model.XMLElement;
 
 public class JSONElementVisitor {
 
+	public static String XMLOBJECT_CANNOT_BE_NULL = "XML Object cannot be null";
+	
 	String elementToString = "";
 	String path = "";
 
 	public String printElement(XMLElement element){
-		
-	//	Objects.requireNonNull(element, Constants.XMLOBJECT_CANNOT_BE_NULL.getConstant());
 
-		if(element.getTagName().equals("root")){
-				for(XMLElement elem: element.getChildren()){
-					elementToString = printElement(elem);
-					path="";
-				}
+		Objects.requireNonNull(element, XMLOBJECT_CANNOT_BE_NULL);
+
+		if(element.getTagName()==null){
+			for(XMLElement elem: element.getChildren()){
+				elementToString = printElement(elem);
+				path="";
+			}
 		}
 		else{
 			//path da stampare
-			if(path.equals(""))
+			if(path.isEmpty())
 				path=element.getTagName();
 			else
 				path += ", " + element.getTagName();
@@ -28,8 +33,8 @@ public class JSONElementVisitor {
 			//l'elemento ha figli
 			if(!element.getChildren().isEmpty()){
 				//l'elemento ha attributi
-			//	if(element.checkAttributes())
-			//		printAttributes(element);
+				if(!element.getAttributes().isEmpty())
+					printAttributes(element);
 				for(XMLElement elementSon : element.getChildren()){
 					elementToString = printElement(elementSon);
 					path = path.replaceFirst(", "+elementSon.getTagName(), "");
@@ -39,9 +44,9 @@ public class JSONElementVisitor {
 				//stampa valore
 				printValue(element);
 				//stampa attributi
-//				if(element.checkAttributes()){
-//					printAttributes(element);
-//				}
+				if(!element.getAttributes().isEmpty()){
+					printAttributes(element);
+				}
 			}
 		}
 		return elementToString;
@@ -55,5 +60,11 @@ public class JSONElementVisitor {
 		}
 	}
 
+	public void printAttributes(XMLElement element) {
+		elementToString += "attributes:\n";
+		for(Map.Entry<String, String> entry : element.getAttributes().entrySet()) {
+			elementToString += entry.getKey()  + " = \"" + entry.getValue() + "\"\n";
+		}
+	}
 
 }
---
+++ src/test/java/eu/tasgroup/hyperskill/jsonparser/visitor/JSONElementVisitorTest.java
@@ -0,0 +1,280 @@
+package eu.tasgroup.hyperskill.jsonparser.visitor;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import eu.tasgroup.hyperskill.jsonparser.model.XMLElement;
+
+class JSONElementVisitorTest {
+	JSONElementVisitor sut = new JSONElementVisitor();
+
+	//null al metodo
+	@Test
+	@DisplayName("visit null element")
+	public void xmlElementNullTest(){
+
+		assertThatThrownBy(() -> sut.printElement(null))
+		.isInstanceOf(NullPointerException.class)
+		.hasMessage(JSONElementVisitor.XMLOBJECT_CANNOT_BE_NULL);
+
+	}
+
+	@Test
+	@DisplayName("print only one tag")
+	public void elementVisitorOneTagNoValueNoAttributesTest() {
+		XMLElement xmlElement = new XMLElement();
+		xmlElement.setTagName("id");
+		xmlElement.setText("");
+	
+		String stringExpected =  "\nElement:\npath = id\nvalue = \"\"\n";
+		String stringVisitor = sut.printElement(xmlElement);
+
+		assertThat(stringVisitor).isEqualToNormalizingNewlines(stringExpected);
+	}
+
+	@Test
+	@DisplayName("print one tag one attribute")
+	public void elementVisitorOneTagNoValueOneAttributeTest() {
+		XMLElement xmlElement = new XMLElement();
+		xmlElement.setTagName("id");
+		xmlElement.setText("");
+		xmlElement.insertAttributeEntry("region", "Russia");
+	
+		String stringExpected =  "\nElement:\npath = id" 
+				+ "\nvalue = \"\"\n" 
+				+ "attributes:\nregion = \"Russia\"\n"; 		
+		String stringVisitor = sut.printElement(xmlElement);
+
+		assertThat(stringVisitor).isEqualToNormalizingNewlines(stringExpected);
+	}
+
+	@Test
+	@DisplayName("print one tag two attributes")
+	public void elementVisitorOneTagNoValueMultipleAttributesTest() {
+		XMLElement xmlElement = new XMLElement();
+		xmlElement.setTagName("id");
+		xmlElement.setText("");
+		xmlElement.insertAttributeEntry("region", "Russia");
+		xmlElement.insertAttributeEntry("region1", "Russia1");
+		
+		String stringExpected =  "\nElement:\npath = id"
+				+ "\nvalue = \"\"\n" 
+				+ "attributes:\nregion = \"Russia\"\n"
+				+ "region1 = \"Russia1\"\n";
+		String stringVisitor = sut.printElement(xmlElement);
+
+		assertThat(stringVisitor).isEqualToNormalizingNewlines(stringExpected);
+	}
+
+	@Test
+	@DisplayName("print one tag with value")
+	public void elementVisitorOneTagOneValueNoAttributesTest() {
+		XMLElement xmlElement = new XMLElement();
+		xmlElement.setTagName("id");
+		xmlElement.setText("6753322");
+		
+		String stringExpected =  "\nElement:\npath = id\nvalue = \"6753322\"\n";
+		String stringVisitor = sut.printElement(xmlElement);
+
+		assertThat(stringVisitor).isEqualToNormalizingNewlines(stringExpected);
+	}
+
+	@Test
+	@DisplayName("print one tag with value and one attribute")
+	public void elementVisitorOneTagOneValueOneAttributeTest() {
+		XMLElement xmlElement = new XMLElement();
+		xmlElement.setTagName("id");
+		xmlElement.setText("6753322");
+		xmlElement.insertAttributeEntry("region", "Russia");
+		
+		String stringExpected =  "\nElement:\npath = id" 
+				+ "\nvalue = \"6753322\"\n" 
+				+ "attributes:\nregion = \"Russia\"\n"; 		
+		String stringVisitor = sut.printElement(xmlElement);
+
+		assertThat(stringVisitor).isEqualToNormalizingNewlines(stringExpected);
+	}
+	
+	@Test
+	@DisplayName("no print root")
+	public void elementVisitorDocumentElementTest() {
+		XMLElement xmlElement = new XMLElement();
+		XMLElement xmlElementChild = new XMLElement();
+		xmlElementChild.setTagName("id");
+		xmlElementChild.setText("6753322");
+		xmlElementChild.insertAttributeEntry("region", "Russia");
+		xmlElement.addChild(xmlElementChild);
+		
+		String stringExpected =  "\nElement:\npath = id" 
+				+ "\nvalue = \"6753322\"\n" 
+				+ "attributes:\nregion = \"Russia\"\n"; 		
+		String stringVisitor = sut.printElement(xmlElement);
+
+		assertThat(stringVisitor).isEqualToNormalizingNewlines(stringExpected);
+	}
+
+	@Test
+	@DisplayName("print one tag with value and two attributes")
+	public void elementVisitorOneTagOneValueMultipleAttributesTest() {
+		XMLElement xmlElement = new XMLElement();
+		xmlElement.setTagName("id");
+		xmlElement.setText("123");
+		xmlElement.insertAttributeEntry("region", "Russia");
+		xmlElement.insertAttributeEntry("region1", "Russia1");
+
+		String stringExpected =  "\nElement:\npath = id" 
+				+ "\nvalue = \"123\"\n" 
+				+ "attributes:\nregion = \"Russia\"\n"
+				+ "region1 = \"Russia1\"\n";
+		String stringVisitor = sut.printElement(xmlElement);
+
+		assertThat(stringVisitor).isEqualToNormalizingNewlines(stringExpected);
+	}
+
+	@Test
+	@DisplayName("print one tag with null value")
+	public void elementVisitorOneTagNullValueNoAttributesTest() {
+		XMLElement xmlElement = new XMLElement();
+		xmlElement.setTagName("id");
+		xmlElement.setText("null");
+		
+		String stringExpected =  "\nElement:\npath = id\nvalue = null\n";
+		String stringVisitor = sut.printElement(xmlElement);
+
+		assertThat(stringVisitor).isEqualToNormalizingNewlines(stringExpected);
+	}
+
+	@Test
+	@DisplayName("print innested tags with value")
+	public void elementVisitorInnestedTagsNoAttributesTest() {
+		XMLElement xmlElementChild = new XMLElement();
+		xmlElementChild.setTagName("to");
+		xmlElementChild.setText("to_example@gmail.com");
+		XMLElement xmlElement = new XMLElement();
+		xmlElement.setTagName("email");
+		xmlElement.addChild(xmlElementChild);
+		
+		String stringPath = "\nElement:\npath = email\n\nElement:\npath = email, to"; 
+		String stringValue = "\nvalue = \"to_example@gmail.com\"\n";
+
+		String stringExpected = stringPath + stringValue;
+		String stringVisitor = sut.printElement(xmlElement);
+
+		assertThat(stringVisitor).isEqualToNormalizingNewlines(stringExpected);
+	}
+
+	@Test
+	@DisplayName("print innested tags with value and attribute")
+	public void elementVisitorInnestedTagsOneAttributeTest() {
+		XMLElement xmlElementChild = new XMLElement();
+		xmlElementChild.setTagName("to");
+		xmlElementChild.setText("to_example@gmail.com");
+		xmlElementChild.insertAttributeEntry("region", "Russia");
+		XMLElement xmlElement = new XMLElement();
+		xmlElement.setTagName("email");
+		xmlElement.addChild(xmlElementChild);
+
+		String stringPath = "\nElement:\npath = email\n\nElement:\npath = email, to"; 
+		String stringValue = "\nvalue = \"to_example@gmail.com\"\n";
+		String stringAttributes = "attributes:\nregion = \"Russia\"\n";
+
+		String stringExpected = stringPath + stringValue + stringAttributes;
+		String stringVisitor = sut.printElement(xmlElement);
+
+		assertThat(stringVisitor).isEqualToNormalizingNewlines(stringExpected);
+	}
+
+	@Test
+	@DisplayName("print innested tags with value and multiple attributes")
+	public void elementVisitorInnestedTagsMultipleAttributesTest() {
+		XMLElement xmlElementChild = new XMLElement();
+		xmlElementChild.setTagName("to");
+		xmlElementChild.setText("to_example@gmail.com");
+		xmlElementChild.insertAttributeEntry("region", "Russia");
+		xmlElementChild.insertAttributeEntry("region1", "Russia1");
+		XMLElement xmlElement = new XMLElement();
+		xmlElement.setTagName("email");
+		xmlElement.addChild(xmlElementChild);
+
+		String stringPath = "\nElement:\npath = email\n\nElement:\npath = email, to"; 
+		String stringValue = "\nvalue = \"to_example@gmail.com\"\n";
+		String stringAttributes = "attributes:\nregion = \"Russia\"\nregion1 = \"Russia1\"\n";
+
+		String stringExpected = stringPath + stringValue + stringAttributes;
+		String stringVisitor = sut.printElement(xmlElement);
+
+		assertThat(stringVisitor).isEqualToNormalizingNewlines(stringExpected);
+	}
+
+	@Test
+	@DisplayName("print innested tags with father with attribute")
+	public void elementVisitorTagFatherOneAttributeTest() {
+		XMLElement xmlElementChild = new XMLElement();
+		xmlElementChild.setTagName("to");
+		xmlElementChild.setText("to_example@gmail.com");
+		XMLElement xmlElement = new XMLElement();
+		xmlElement.setTagName("email");
+		xmlElement.addChild(xmlElementChild);
+		xmlElement.insertAttributeEntry("region", "Russia");
+		
+		String stringPathFather = "\nElement:\npath = email\n";
+		String attributeFather = "attributes:\nregion = \"Russia\"\n";
+		String stringPathSon = "\nElement:\npath = email, to";
+		String stringValue = "\nvalue = \"to_example@gmail.com\"\n";
+
+		String stringExpected = stringPathFather + attributeFather + stringPathSon + stringValue;
+		String stringVisitor = sut.printElement(xmlElement);
+
+		assertThat(stringVisitor).isEqualToNormalizingNewlines(stringExpected);
+	}
+
+	@Test
+	@DisplayName("print innested tags with father with multiple attributes")
+	public void elementVisitorTagFatherMultipleAttributesTest() {
+		XMLElement xmlElementChild = new XMLElement();
+		xmlElementChild.setTagName("to");
+		xmlElementChild.setText("to_example@gmail.com");
+		XMLElement xmlElement = new XMLElement();
+		xmlElement.setTagName("email");
+		xmlElement.addChild(xmlElementChild);
+		xmlElement.insertAttributeEntry("region", "Russia");
+		xmlElement.insertAttributeEntry("region1", "Russia1");
+
+		String stringPathFather = "\nElement:\npath = email\n";
+		String attributeFather = "attributes:\nregion = \"Russia\"\nregion1 = \"Russia1\"\n";
+		String stringPathSon = "\nElement:\npath = email, to";
+		String stringValue = "\nvalue = \"to_example@gmail.com\"\n";
+
+		String stringExpected = stringPathFather + attributeFather + stringPathSon + stringValue;
+		String stringVisitor = sut.printElement(xmlElement);
+
+		assertThat(stringVisitor).isEqualToNormalizingNewlines(stringExpected);
+	}
+
+	@Test
+	@DisplayName("print innested tags with father and child with attribute")
+	public void elementVisitorTagFatherTagSonOneAttributeTest() {
+		XMLElement xmlElementChild = new XMLElement();
+		xmlElementChild.setTagName("to");
+		xmlElementChild.setText("to_example@gmail.com");
+		xmlElementChild.insertAttributeEntry("region1", "Russia1");
+		XMLElement xmlElement = new XMLElement();
+		xmlElement.setTagName("email");
+		xmlElement.addChild(xmlElementChild);
+		xmlElement.insertAttributeEntry("region", "Russia");
+		
+		String stringPathFather = "\nElement:\npath = email\n";
+		String attributeFather = "attributes:\nregion = \"Russia\"\n";
+		String stringPathSon = "\nElement:\npath = email, to";
+		String stringValue = "\nvalue = \"to_example@gmail.com\"\n";
+		String attributeSon = "attributes:\nregion1 = \"Russia1\"\n";
+
+		String stringExpected = stringPathFather + attributeFather + stringPathSon + stringValue + attributeSon;
+		String stringVisitor = sut.printElement(xmlElement);
+
+		assertThat(stringVisitor).isEqualToNormalizingNewlines(stringExpected);
+	}
+}
